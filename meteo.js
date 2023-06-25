6440810505def01433de1c2a1364bc3486c483ad
Pouki8 <137617683+Pouki8@users.noreply.github.com>
2023-06-25 17:47:21 +0200

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -14,28 +14,6 @@ fetch("conf.json")
       .catch(error => {
         console.log("Une erreur s'est produite lors de la récupération des données météo :", error);
       });
-    
-    // Mise à jour du nom de la ville en cas de modification dans le fichier JSON
-    setInterval(() => {
-      fetch("conf.json")
-        .then(response => response.json())
-        .then(data => {
-          city = data.city;
-          const updatedApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=fr`;
-
-          fetch(updatedApiUrl)
-            .then(response => response.json())
-            .then(data => {
-              updateWeather(data);
-            })
-            .catch(error => {
-              console.log("Une erreur s'est produite lors de la récupération des données météo :", error);
-            });
-        })
-        .catch(error => {
-          console.log("Une erreur s'est produite lors de la récupération de la configuration :", error);
-        });
-    }, 60000); // Mettre à jour toutes les heures (60000 millisecondes)
   })
   .catch(error => {
     console.log("Une erreur s'est produite lors de la récupération de la configuration :", error);
