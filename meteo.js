acd7e8ffe8a1cbb8d576dcdd76eb32aab638d82e
Pouki8 <137617683+Pouki8@users.noreply.github.com>
2023-06-25 19:18:25 +0200

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,46 +1,34 @@
-fetch("conf.json")
-  .then(response => response.json())
-  .then(data => {
-    const apiKey = data.apiKey;
-    let city = data.city;
+// Fonction pour récupérer et mettre à jour la météo
+function fetchWeather() {
+  fetch("conf.json")
+    .then(response => response.json())
+    .then(data => {
+      const apiKey = data.apiKey;
+      const city = data.city;
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=fr`;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=fr`;
 
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        updateWeather(data);
-      })
-      .catch(error => {
-        console.log("Une erreur s'est produite lors de la récupération des données météo :", error);
-      });
-
-    // Mise à jour du nom de la ville en cas de modification dans le fichier JSON
-    setInterval(() => {
-      fetch("conf.json")
+      fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-          city = data.city;
-          const updatedApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=fr`;
-
-          fetch(updatedApiUrl)
-            .then(response => response.json())
-            .then(data => {
-              updateWeather(data);
-            })
-            .catch(error => {
-              console.log("Une erreur s'est produite lors de la récupération des données météo :", error);
-            });
+          updateWeather(data);
         })
         .catch(error => {
-          console.log("Une erreur s'est produite lors de la récupération de la configuration :", error);
+          console.log("Une erreur s'est produite lors de la récupération des données météo :", error);
         });
-    }, 60000); // Mettre à jour toutes les minutes (60000 millisecondes)
-  })
-  .catch(error => {
-    console.log("Une erreur s'est produite lors de la récupération de la configuration :", error);
-  });
+    })
+    .catch(error => {
+      console.log("Une erreur s'est produite lors de la récupération de la configuration :", error);
+    });
+}
 
+// Mettre à jour la météo immédiatement
+fetchWeather();
+
+// Mettre à jour la météo toutes les heures (3600 secondes)
+setInterval(fetchWeather, 3600000);
+
+// Fonction pour mettre à jour les informations de la météo dans le DOM
 function updateWeather(data) {
   const cityName = data.name;
   const temperature = (data.main.temp - 273.15).toFixed(1);
