207a958c6d9d3d8df570ff0a54f95e4b689c3d48
Pouki8 <137617683+Pouki8@users.noreply.github.com>
2023-06-25 19:38:12 +0200

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -5,7 +5,7 @@ fetch('conf.json')
         const city = config.city; // Récupérer le nom de la ville à partir de la configuration
 
         // Appeler l'API météo avec le nom de la ville
-        const apiKey = 'YOUR_API_KEY'; // Remplacer par votre clé d'API météo
+        const apiKey = 'a2aacfdae1f99a5372617b466f045d34'; // clé d'API météo
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         // Récupérer les données météo depuis l'API
